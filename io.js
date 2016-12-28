a753954f766b06309793e9fff8f57194885e1c20
moncatlab <moncat39@gmail.com>
2016-12-28 22:32:59 +0800

    Create io.js
    Add io.js

---
+++ io.js
@@ -0,0 +1,63 @@
+{
+    "code":200,
+    "ver":1,
+    "step":5,
+
+    "control":{
+    	"os":{},
+    	"splash":{},
+    	"aw":{},
+    	"sl":{},
+    	"ft":{},
+    	"ins":{},
+    	"pop":{}
+    },
+    "places":{
+    	"sl":{
+    		"placements":[
+    			{"pf":7, "id":""},
+		        {"pf":7, "id":""},
+		        {"pf":1, "id":""},
+		        {"pf":0, "id":""}
+    		]
+    	}.
+    	"sln":{
+    		"placements":[
+    			{"pf":7, "id":""},
+		        {"pf":7, "id":""},
+		        {"pf":1, "id":""},
+		        {"pf":0, "id":""}
+    		]
+    	}.
+    	"finish":{
+    		"placements":[
+    			{"pf":7, "id":""},
+		        {"pf":7, "id":""},
+		        {"pf":1, "id":""},
+		        {"pf":0, "id":""}
+    		]
+    	},
+    	"pop":{
+    		"placements":[
+    			{"pf":7, "id":""},
+		        {"pf":7, "id":""},
+		        {"pf":1, "id":""},
+		        {"pf":0, "id":""}
+    		]
+    	},
+    	"desktop":{
+    		"placements":[
+    			{"pf":7, "id":""},
+		        {"pf":7, "id":""},
+		        {"pf":1, "id":""},
+		        {"pf":0, "id":""}
+    		]
+    	},
+    	"splash":{
+    		"placements":[
+    			{"pf":6, "id":""}
+    		]
+    	}
+    }       
+    ]
+}
